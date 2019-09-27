5fbef463302486725bb285bb2e171c92ae014f83
Franz Knipp <franz@qnipp.com>
2019-09-27 17:22:50 +0200

    Add the Meteor app to reproduce the bug on Firefox 60

---
+++ client/main.js
@@ -0,0 +1,10 @@
+console.log(`Greetings from ${module.id}!`);
+
+const object = {
+  one: 1,
+  two: 2,
+};
+
+const array = Object.entries(object);
+const newObject = Object.fromEntries(array);
+console.log(newObject);
